063eacb8cab85acc07608705dd5ae284a74f231a
nhi <duongtranyennhi8d@gmail.com>
2024-03-13 12:36:30 +0700

    #ManagTree: fetch data/ CRUD

---
+++ Webapp/environment-app/src/pages/ManageTree/index.tsx
@@ -0,0 +1,74 @@
+import { Button } from "react-bootstrap";
+import { BiSolidEdit } from "react-icons/bi";
+import { Link, useNavigate } from "react-router-dom";
+import { TREE_LIST } from "../../api";
+import { ListView } from "../../components/ListView";
+import { Column } from "../../components/ListView/Table";
+import ModalDelete from "../../components/Modals/ModalDelete";
+import { dayFormat } from "../../utils";
+
+export const ManageTree = () => {
+  const navigate = useNavigate();
+  // TODO get list
+
+  const columns: Column[] = [
+    {
+      header: "Chỉnh sửa",
+      accessorFn(row) {
+        return (
+          <div>
+            <Link to={`/manage-tree/${row?.treeCode}/update`}>
+              <button type="button" className="btn btn-click">
+                <BiSolidEdit />
+              </button>
+            </Link>
+            <button onClick={() => {}}>
+              <ModalDelete />
+            </button>
+          </div>
+        );
+      },
+    },
+    {
+      header: "Mã số cây",
+      accessorFn(row) {
+        return <Link to={`/manage-tree/${row.treeCode}`}>{row.treeCode}</Link>;
+      },
+    },
+    { header: "Tuyến đường", accessorKey: "streetName", align: "left" },
+    { header: "Giống cây", accessorKey: "cultivar", align: "left" },
+    { header: "Đường kính thân", accessorKey: "bodyDiameter", align: "left" },
+    { header: "Tán lá", accessorKey: "leafLength", align: "left" },
+    {
+      header: "Thời điểm cắt tỉa gần nhất",
+      accessorFn(row) {
+        return <h6>{dayFormat(row.cutTime)}</h6>;
+      },
+    },
+    {
+      header: "Trạng thái",
+      accessorFn(row) {
+        const status = row.isCut ? "Đã cắt" : "Cần Cắt";
+        const color = row.isCut ? "green" : "red";
+        return <span style={{ color, fontWeight: "bold" }}>{status}</span>;
+      },
+    },
+  ];
+
+  return (
+    <div>
+      <ListView
+        listURL={TREE_LIST}
+        columns={columns}
+        bottom={
+          <Button
+            variant="success"
+            onClick={() => navigate("/manage-tree/create")}
+          >
+            Thêm
+          </Button>
+        }
+      />
+    </div>
+  );
+};
