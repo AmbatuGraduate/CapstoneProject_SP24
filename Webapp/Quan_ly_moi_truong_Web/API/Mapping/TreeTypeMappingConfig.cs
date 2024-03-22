2218fc51b2be5d752eaa7f88b06be63a0bddf01e
Dang Nguyen Khanh Vu <83583888+khvu443@users.noreply.github.com>
2024-03-22 20:16:35 +0700

    Update TreeTypeMappingConfig.cs

--- Webapp/Quan_ly_moi_truong_Web/API/Mapping/TreeTypeMappingConfig.cs
+++ Webapp/Quan_ly_moi_truong_Web/API/Mapping/TreeTypeMappingConfig.cs
@@ -20,7 +20,8 @@ namespace API.Mapping
                   .MapWith(dest => new UpdateTreeTypeCommand(dest.Item1, dest.Item2.TreeTypeName, dest.Item2.UpdateBy));
 
             config.NewConfig<TreeTypeResult, ListTreeTypeResponse>()
-                  .Map(dest => dest.TreeTypeName, src => src.treeTypeResults.TreeTypeName);
+                  .Map(dest => dest.TreeTypeName, src => src.treeTypeResults.TreeTypeName)
+                  .Map(dest => dest.TreeTypeId, src => src.treeTypeResults.TreeTypeId);
         }
     }
 }
