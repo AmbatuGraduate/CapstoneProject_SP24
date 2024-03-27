7b00ebf3e1fcc25162538f1ad32ee73e1f0a63be
nhi <duongtranyennhi8d@gmail.com>
2024-03-27 22:00:30 +0700

    Fix UI for sidebar

--- Webapp/environment-app/src/Components/SideBar/index.tsx
+++ Webapp/environment-app/src/Components/SideBar/index.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import "./style.scss";
 
 // Imported Icons
@@ -8,11 +9,18 @@ import { GrUserManager } from "react-icons/gr";
 import { LuTrees } from "react-icons/lu";
 import { PiTrashSimpleBold } from "react-icons/pi";
 import { TbLayoutDashboard } from "react-icons/tb";
+import { IoHomeOutline } from "react-icons/io5";
 
 // import router
 import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
+  const [showCard, setShowCard] = useState(false);
+
+  const toggleCard = () => {
+    setShowCard(!showCard);
+  };
+
   return (
     <div className="sideBar ">
       <div className="logoDiv flex">
@@ -21,6 +29,15 @@ const Sidebar = () => {
       <hr className="line" />
       <div className="menuDiv p-0">
         <ul className="menuLists grid">
+          <li className="listItem">
+            <NavLink to="manage-report" className="menuLink flex">
+              <IoHomeOutline className="icon" />
+              <span className="smallText d-none d-md-inline ">
+                Trang chủ
+              </span>
+            </NavLink>
+          </li>
+
           <li className="listItem">
             <NavLink to="manage-report" className="menuLink flex">
               <TbLayoutDashboard className="icon" />
@@ -58,7 +75,7 @@ const Sidebar = () => {
           </li>
 
           <li className="listItem">
-            <NavLink to="/manage-clean-schedule" className="menuLink flex">
+            <NavLink to="/manage-cleaning-schedule" className="menuLink flex">
               <FaTreeCity className="icon" />
               <span className="smallText d-none d-md-inline">
                 Quản lý lịch vệ sinh đô thị
@@ -67,7 +84,10 @@ const Sidebar = () => {
           </li>
 
           <li className="listItem">
-            <NavLink to="/" className="menuLink flex">
+            <NavLink
+              to="/manage-garbagecollection-schedule"
+              className="menuLink flex"
+            >
               <PiTrashSimpleBold className="icon" />
               <span className="smallText d-none d-md-inline">
                 Quản lý lịch thu gom rác
@@ -78,18 +98,19 @@ const Sidebar = () => {
       </div>
 
       <div className="sideBarCard">
-        <BsQuestionCircle className="icon" />
-        <div className="cardContent d-none d-xl-block">
-          <div className="circle1"></div>
-          <div className="circle2"></div>
-
-          <h3>Trung tâm trợ giúp</h3>
-          <p>
-            Nếu bạn gặp vấn đề khi sử dụng Ambatu, xin hãy liên hệ với chúng tôi
-            để nhận hỗ trợ.
-          </p>
-          <button className="btn">Liên hệ trung tâm</button>
-        </div>
+        <BsQuestionCircle className="icon" onClick={toggleCard} />
+        {showCard && (
+          <div className="cardContent">
+            <div className="circle1"></div>
+            <div className="circle2"></div>
+            <h3>Trung tâm trợ giúp</h3>
+            <p>
+              Nếu bạn gặp vấn đề khi sử dụng Ambatu, xin hãy liên hệ với chúng
+              tôi để nhận hỗ trợ.
+            </p>
+            <button className="btn">Liên hệ trung tâm</button>
+          </div>
+        )}
       </div>
     </div>
   );
