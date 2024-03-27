e76e8aeca3495ac125ccf6021851badee5aeed5a
nhi <duongtranyennhi8d@gmail.com>
2024-03-27 21:49:10 +0700

    Feature: add Cleaning schedule

--- Webapp/environment-app/src/utils/index.ts
+++ Webapp/environment-app/src/utils/index.ts
@@ -35,3 +35,22 @@ function getCookie(cname) {
   }
   return "";
 }
+
+export const timeFormat = (day: string | Date) => {
+  return dayjs(day).format("HH:mm");
+};
+
+export const taskStatus = (status) => {
+  switch (status) {
+    case "None":
+      return { text: "", color: "" };
+    case "Not Start":
+      return { text: "Chưa Thực Hiện", color: "red" };
+    case "In Progress":
+      return { text: "Đang Làm", color: "orange" };
+    case "Done":
+      return { text: "Đã Hoàn Thành", color: "green" };
+    default:
+      return { text: status, color: "" };
+  }
+};
