2f98eaaddcc89663c7feb0f459d4322907d23fda
Lê Anh Quân <quanrealvn@gmail.com>
2024-03-30 18:45:11 +0700

    add more user fields

--- Webapp/Quan_ly_moi_truong_Web/Application/User/Common/List/GoogleUser.cs
+++ Webapp/Quan_ly_moi_truong_Web/Application/User/Common/List/GoogleUser.cs
@@ -6,5 +6,8 @@
         public string Email { get; set; }
         public string Name { get; set; }
         public string Picture { get; set; }
+        public string Department { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Role { get; set; }
     }
 }
