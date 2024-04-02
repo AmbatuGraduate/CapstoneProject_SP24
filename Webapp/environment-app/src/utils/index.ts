d603ddc7c71303038e71a948bc79e3a61a788edd
nhi <duongtranyennhi8d@gmail.com>
2024-04-03 00:45:24 +0700

    Add: Create and detail page

--- Webapp/environment-app/src/utils/index.ts
+++ Webapp/environment-app/src/utils/index.ts
@@ -1,4 +1,3 @@
-import { time } from "console";
 import dayjs from "dayjs";
 
 export const dayFormat = (day: string | Date) => {
@@ -7,9 +6,8 @@ export const dayFormat = (day: string | Date) => {
 
 export const dateConstructor = (day: string) => {
   const [d, m, y] = day.split("/").map(Number);
-  return new Date(y, m - 1, d)
+  return new Date(y, m-1, d)
 }
-
 
 export const timeFormat = (day: string | Date) => {
   return dayjs(day).format("HH:mm");
@@ -20,14 +18,42 @@ export const taskStatus = (status) => {
     case "None":
       return { text: "", color: "" };
     case "Not Start":
-      return { text: "Chưa Thực Hiện", color: "black" };
+      return { text: "Chưa Thực Hiện", color: "red" };
     case "In Progress":
       return { text: "Đang Làm", color: "orange" };
     case "Done":
       return { text: "Đã Hoàn Thành", color: "green" };
-    case "Late":
-      return { text: "Đã Trễ", color: "red" };
     default:
       return { text: status, color: "" };
   }
 };
+
+const ReportImpactType = {
+  LOW: 0,
+  MEDIUM: 1,
+  HIGH: 2
+}
+
+
+export const ReportImpact = (reportImpact) => {
+  switch (reportImpact) {
+    case ReportImpactType.LOW:
+      return { text: "Thấp", color: "grey"};
+    case ReportImpactType.MEDIUM:
+      return {text: "Trung bình", color: "orange"};
+    case ReportImpactType.HIGH:
+      return {text: "Cao", color: "red"};
+    default:
+      return { text: reportImpact, color: "" };
+  }
+}
+
+export const ReportStatus = (reportStatus: string): {text:string, color: string}=> {
+  switch (reportStatus) {
+    case "UnResolved":
+      return {text: "Chưa được xử lý", color: "red"};
+    case "Resolved":
+      return {text: "Đã được xử lý", color: "green"};
+    default: return {text: "Chưa được xử lý", color: "red"};
+  }
+}
