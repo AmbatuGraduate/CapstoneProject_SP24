acfc131e1f2caa55618015247f4369f0859d542c
nhi <duongtranyennhi8d@gmail.com>
2024-04-03 00:45:01 +0700

    Display: report list

--- Webapp/environment-app/src/pages/ManageReport/index.tsx
+++ Webapp/environment-app/src/pages/ManageReport/index.tsx
@@ -1,55 +1,100 @@
 import { Button } from "react-bootstrap";
-import { BiSolidEdit } from "react-icons/bi";
 import { Link, useNavigate } from "react-router-dom";
-import { REPORT_LIST, TREE_DELETE, TREE_LIST, useApi } from "../../Api";
+import { REPORT_LIST } from "../../Api";
 import { ListView } from "../../Components/ListView";
 import { Column } from "../../Components/ListView/Table";
-import { dayFormat } from "../../utils";
-import ModalDelete from "../../Components/Modals/ModalDelete";
+import { ReportImpact, ReportStatus, dayFormat } from "../../utils";
 import { useRef } from "react";
+
+import { MdAddCircleOutline } from "react-icons/md";
 
 export const ManageReport = () => {
   const navigate = useNavigate();
   const ref = useRef<any>();
   // TODO get list
 
-  const handleDelete = async (id: string) => {
-    await useApi.delete(TREE_DELETE.replace(":id", id));
-    ref.current?.reload();
-  };
+  // const handleDelete = async (id: string) => {
+  //   await useApi.delete(TREE_DELETE.replace(":id", id));
+  //   ref.current?.reload();
+  // };
 
   const columns: Column[] = [
+    // {
+    //   header: "Chỉnh sửa",
+    //   accessorFn(row) {
+    //     return (
+    //       <div>
+    //         <button type="button" className="btn btn-click" onClick={() => {}}>
+    //           <ModalDelete handleDelete={() => handleDelete(row.id)} />
+    //         </button>
+    //       </div>
+    //     );
+    //   },
+    // },
     {
-      header: "Chỉnh sửa",
-      accessorFn(row) {
+      header: "Người gửi",
+      accessorFn(longRow) {
         return (
-          <div>
-            <Link to={`/manage-report/${row?.id}/update`}>
-              <button type="button" className="btn btn-click">
-                <BiSolidEdit />
-              </button>
+          <h6 className="shortText">
+            <Link
+              className="linkCode"
+              style={{ fontWeight: "bold" }}
+              to={`/manage-report/${longRow.id}`}
+            >
+              {longRow.issuerEmail}
             </Link>
-            <button type="button" className="btn btn-click" onClick={() => { }}>
-              <ModalDelete handleDelete={() => handleDelete(row.id)} />
-            </button>
-          </div>
+          </h6>
         );
       },
+      width: "25%",
     },
     {
-      header: "Mã báo cáo",
+      header: "Trạng thái",
       accessorFn(row) {
         return (
-          <Link className="linkCode" to={`/manage-report/${row.id}`}>
-            {row.treeCode}
-          </Link>
+          <h6
+            className="shortText"
+            style={{
+              color: ReportStatus(row.reportStatus).color,
+              fontWeight: "bold",
+            }}
+          >
+            {ReportStatus(row.reportStatus).text}
+          </h6>
         );
       },
+      width: "15%",
     },
-    { header: "Tiêu đề", accessorKey: "streetName", },
-    { header: "Người gửi", accessorKey: "cultivar", },
-    { header: "Mức độ ảnh hưởng", accessorKey: "bodyDiameter", },
-    { header: "Trạng thái", accessorKey: "leafLength", },
+    {
+      header: "Mức độ ảnh hưởng",
+      accessorFn(row) {
+        return (
+          <h6
+            className="shortText"
+            style={{
+              color: ReportImpact(row.reportImpact).color,
+              fontWeight: "bold",
+            }}
+          >
+            {ReportImpact(row.reportImpact).text}
+          </h6>
+        );
+      },
+      width: "20%",
+    },
+    {
+      header: "Cần giải quyết trước",
+      accessorFn(row) {
+        return <h6 className="shortText">{dayFormat(row.expectedResolutionDate)}</h6>;
+      },
+      width: "15%",
+    },
+    {
+      header: "Ngày đã giải quyết",
+      accessorFn(row) {
+        return <h6 className="shortText">{dayFormat(row.actualResolutionDate)}</h6>;
+      },
+    },
   ];
 
   return (
@@ -58,9 +103,21 @@ export const ManageReport = () => {
         ref={ref}
         listURL={REPORT_LIST}
         columns={columns}
-        filter={(row) => {
-          return row.isExist == true;
-        }}
+        bottom={
+          <Button
+            variant="success"
+            style={{
+              backgroundColor: "hsl(94, 59%, 35%)",
+              border: "none",
+              padding: "0.5rem 1rem",
+            }}
+            onClick={() => navigate("create")}
+          >
+            <MdAddCircleOutline className="iconAdd" />
+            Thêm báo cáo
+          </Button>
+        }
+        transform={(data: any) => data?.value?.map((i) => i.reportFormat) || []}
       />
     </div>
   );
