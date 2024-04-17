da250013f7c0954561d0100ab81108984be6967f
Dang Nguyen Khanh Vu <165540447+kvu443@users.noreply.github.com>
2024-04-17 23:32:20 +0700

    Update axiosClient.ts

--- Webapp/environment-app/src/Api/axiosClient.ts
+++ Webapp/environment-app/src/Api/axiosClient.ts
@@ -30,8 +30,8 @@ axiosClient.interceptors.request.use(
     console.log('compare - expire > now', expire > Date.now());
 
     // if (expire < Date.now()) {
-      // const response = await axios.get('https://vesinhdanang.xyz:7024/api/auth/refresh',
-        const response = await axios.get('https://localhost:7024/api/auth/refresh',
+      const response = await axios.get('https://vesinhdanang.xyz:7024/api/auth/refresh',
+        // const response = await axios.get('https://localhost:7024/api/auth/refresh',
         {
           withCredentials: true,
           headers: {
